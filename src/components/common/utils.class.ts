d18c08ce61e8df76cd4f0511a28a318254d9e284
Azarattum <azarattum@pm.me>
2021-03-27 23:09:12 +0700

    Added shuffle util

--- src/components/common/utils.class.ts
+++ src/components/common/utils.class.ts
@@ -165,6 +165,20 @@ export default class Utils {
 	public static async sleep(ms: number): Promise<void> {
 		return new Promise(resolve => setTimeout(resolve, ms));
 	}
+
+	/**
+	 * Returns a copy of the array but shuffled
+	 * @param array Array to shuffle
+	 */
+	public static shuffle<T>(array: T[]): T[] {
+		array = [...array];
+		for (let i = array.length - 1; i > 0; i--) {
+			const j = Math.floor(Math.random() * (i + 1));
+			[array[i], array[j]] = [array[j], array[i]];
+		}
+
+		return array;
+	}
 }
 
 /**
@@ -184,4 +198,5 @@ const format = Utils.format;
 const generateID = Utils.generateID;
 const convertTo = Utils.convertTo;
 const sleep = Utils.sleep;
-export { log, format, generateID, convertTo, sleep };
+const shuffle = Utils.shuffle;
+export { log, format, generateID, convertTo, sleep, shuffle };
