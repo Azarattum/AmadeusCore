1c9df2568b2a045079ad88a1a49c3f5c7f143762
Azarattum <azarattum@pm.me>
2021-04-02 17:16:36 +0700

    Adapted soundcloud provider

--- src/components/app/models/providers/soundcloud.provider.ts
+++ src/components/app/models/providers/soundcloud.provider.ts
@@ -1,8 +1,10 @@
-import parse from "../parser";
+import { gretch } from "gretchen";
+import { assertType } from "typescript-is";
+import parse, { parseArtists } from "../parser";
 import { ITrack } from "../track.interface";
 import Provider from "./provider.abstract";
 
-export default class SoundCloudProvider extends Provider {
+export default class SoundCloudProvider extends Provider<ITrackSoundCloud> {
 	protected baseURL = "https://api-v2.soundcloud.com/";
 	protected headers = {
 		"User-Agent":
@@ -12,179 +14,123 @@ export default class SoundCloudProvider extends Provider {
 		client_id: this.token
 	};
 
-	private async search(
+	protected async *identify(
+		source: string
+	): AsyncGenerator<ITrackSoundCloud> {
+		//From aggregator
+		if (source.startsWith("aggr://soundcloud:")) {
+			const audio = await this.call(`tracks/${source.slice(18)}`);
+			const track = assertType<ITrackSoundCloud>(audio);
+			yield track;
+
+			return;
+		}
+	}
+
+	protected async *search(
 		query: string,
 		count = 1,
 		offset = 0
-	): Promise<ITrackSoundCloud[]> {
-		const response = await this.call("search/tracks", {
+	): AsyncGenerator<ITrackSoundCloud> {
+		const data = await this.call("search/tracks", {
 			q: query,
 			limit: count,
 			offset
 		});
-		const json = await response.json();
 
-		return json["collection"] as ITrackSoundCloud[];
+		const tracks = assertType<IResponseSoundCloud>(data).collection;
+		for await (const track of tracks) yield track;
+	}
+
+	protected async convert(
+		track: ITrackSoundCloud
+	): Promise<ITrack | { url: undefined }> {
+		const { title, album, artists, year } = parse(track.title);
+
+		const media = track.media.transcodings
+			.filter(x => x.format.protocol == "progressive")?.[0]
+			?.url?.replace(this.baseURL, "");
+
+		if (!media) return { url: undefined };
+
+		artists.push(
+			...parseArtists(track.publisher_metadata?.artist || undefined)
+		);
+		if (!artists.length)
+			artists.push(track.user.full_name || track.user.username);
+
+		const converted = {
+			title: title,
+			artists: [...new Set(artists)],
+			album: track.publisher_metadata?.album_title || album || title,
+			length: track.full_duration / 1000,
+			year:
+				year ||
+				new Date(track.release_date || track.created_at).getFullYear(),
+			cover: undefined as string | undefined,
+			url: undefined as string | undefined,
+			sources: [`aggr://soundcloud:${track.id}`]
+		};
+
+		const [url, cover] = await this.load(
+			media,
+			track.artwork_url || track.user.avatar_url || undefined
+		);
+
+		converted.cover = cover;
+		converted.url = url;
+
+		return converted;
 	}
 
 	private async load(
-		track: ITrackSoundCloud,
-		cover: string | null
-	): Promise<[string | null, string | null]> {
+		media: string,
+		cover?: string
+	): Promise<[string?, string?]> {
 		if (cover) {
-			const alt = cover.replace("t500x500", "original");
-			cover = (
-				await fetch(alt).catch(() => {
-					return { ok: false };
-				})
-			).ok
-				? alt
-				: cover;
+			const original = cover.replace("large.jpg", "original.jpg");
+			const { status } = await gretch(original).flush();
+
+			if (status === 200) cover = original;
+			else cover = cover.replace("large.jpg", "t500x500.jpg");
+
+			if (cover.includes("default_avatar")) cover = undefined;
 		}
 
-		const codings = track.media.transcodings;
-		const url = codings
-			.filter(x => x.format.protocol == "progressive")?.[0]
-			?.url?.replace(this.baseURL, "");
-		if (!url) return [null, cover];
+		const data = await this.call(media);
+		const url = assertType<{ url: string }>(data).url;
 
-		const reponse = await this.call(url);
-		const json = await reponse.json();
-
-		if (json.url.includes("preview")) return [null, cover];
-		return [json.url || null, cover];
-	}
-
-	public async get(query: string, count = 1, offset = 0): Promise<ITrack[]> {
-		const joins = /,|ft.|feat.|&|\+|\/|featuring|med|\||\band\b/;
-		const tracks = await this.search(query, count, offset);
-
-		const metas = tracks.map(x => {
-			const { title, album, artists, year } = parse(x.title);
-
-			const cover = (x.artwork_url || x.user.avatar_url).replace(
-				"large.jpg",
-				"t500x500.jpg"
-			);
-			const media = x.media.transcodings
-				.filter(x => x.format.protocol == "progressive")?.[0]
-				?.url?.replace(this.baseURL, "");
-
-			return {
-				title: title,
-				artists: [
-					...new Set([
-						...(x.publisher_metadata?.artist
-							?.split(joins)
-							?.map(x => x.trim()) || !artists.length
-							? [x.user.full_name || x.user.username]
-							: []),
-						...artists
-					])
-				],
-				album: x.publisher_metadata?.album_title || album,
-				length: x.full_duration / 1000,
-				year:
-					year ||
-					new Date(x.release_date || x.created_at).getFullYear(),
-				cover: cover.includes("default_avatar") ? undefined : cover,
-				url: null as any,
-				sources: [`aggr://soundcloud:${media}`]
-			};
-		});
-
-		await this.update(
-			tracks.map((x, i) => [x, metas[i].cover]),
-			this.load,
-			(x, i) => {
-				metas[i].url = x[0];
-				metas[i].cover = x[1];
-			}
-		);
-
-		return metas.filter(x => x.url);
-	}
-
-	public async desource(source: string): Promise<string | null> {
-		if (!source.startsWith("aggr://")) return null;
-		source = source.replace("aggr://", "");
-		if (!source.startsWith("soundcloud:")) return null;
-		source = source.replace("soundcloud:", "");
-
-		const json = await (await this.call(source)).json();
-		return json?.url || null;
+		if (url.includes("preview")) return [undefined, cover];
+		return [url, cover];
 	}
 }
 
+interface IResponseSoundCloud {
+	collection: ITrackSoundCloud[];
+}
+
 interface ITrackSoundCloud {
-	artwork_url: string;
-	caption?: any;
-	commentable: boolean;
-	comment_count: number;
+	artwork_url: string | null;
 	created_at: string;
-	description?: any;
-	downloadable: boolean;
-	download_count: number;
-	duration: number;
 	full_duration: number;
-	embeddable_by: string;
-	genre: string;
-	has_downloads_left: boolean;
 	id: number;
-	kind: string;
-	label_name?: any;
-	last_modified: string;
-	license: string;
-	likes_count: number;
-	permalink: string;
-	permalink_url: string;
-	playback_count: number;
-	public: boolean;
-	publisher_metadata: IArtistSoundCloud;
-	purchase_title?: any;
-	purchase_url?: any;
-	release_date: string;
-	reposts_count: number;
-	secret_token?: any;
-	sharing: string;
-	state: string;
-	streamable: boolean;
-	tag_list: string;
+	publisher_metadata: IArtistSoundCloud | null;
+	release_date: string | null;
 	title: string;
-	track_format: string;
-	uri: string;
-	urn: string;
-	user_id: number;
-	visuals?: any;
-	waveform_url: string;
-	display_date: string;
 	media: { transcodings: ITranscodingsSoundCloud[] };
-	monetization_model: string;
-	policy: string;
-	user: any;
+	user: {
+		username: string;
+		full_name?: string | null;
+		avatar_url?: string | null;
+	};
 }
 
 interface ITranscodingsSoundCloud {
 	url: string;
-	preset: string;
-	duration: number;
-	snipped: boolean;
-	format: { protocol: "hls" | "progressive"; mime_type: string };
-	quality: string;
+	format: { protocol: "hls" | "progressive" };
 }
 
 interface IArtistSoundCloud {
-	id: number;
-	urn: string;
-	artist: string;
-	album_title: string;
-	contains_music: boolean;
-	upc_or_ean: string;
-	isrc: string;
-	explicit: boolean;
-	p_line: string;
-	p_line_for_display: string;
-	c_line: string;
-	c_line_for_display: string;
-	release_title: string;
+	artist?: string | null;
+	album_title?: string | null;
 }
