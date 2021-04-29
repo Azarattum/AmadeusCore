0b446a41c8ec0078ab81d965d9d51b40795dde03
Azarattum <azarattum@pm.me>
2021-04-29 23:01:36 +0700

    Refactored tg base. Added callback support

--- src/components/app/controllers/endpoints/telegram.base.ts
+++ src/components/app/controllers/endpoints/telegram.base.ts
@@ -1,7 +1,9 @@
-import { log, LogType, sleep } from "../../../common/utils.class";
-import Form from "../../models/form";
+import { err, sleep, wrn } from "../../../common/utils.class";
 import Tenant from "../../models/tenant";
 import Endpoint from "./endpoint.abstract";
+import { assertType, is } from "typescript-is";
+import Form from "../../models/form";
+import { gretch } from "gretchen";
 import AbortController from "abort-controller";
 
 export default abstract class TelegramBase extends Endpoint {
@@ -9,113 +11,142 @@ export default abstract class TelegramBase extends Endpoint {
 
 	protected abstract onMessage(message: string): void;
 	protected abstract onCommand(command: string): void;
+	protected abstract onTagged(channel: string): void;
+	protected abstract onPost(text: string, channel: string): void;
 	protected abstract onChat(
 		id: number,
 		title: string,
 		description?: string
 	): void;
+	protected abstract onCallback(
+		data: ICallbackData,
+		id: string,
+		message: number,
+		chat: number
+	): void;
 
 	private static url: string;
 	private static inited = false;
+	private static username: string;
+	private static globalAbort: AbortController;
 	private static clients: Map<number, TelegramBase> = new Map();
-	private static abortController: AbortController;
 
 	public static get relations(): Tenant[] {
 		return Tenant.tenants.filter(x => x.telegram);
+	}
+
+	protected static async initialize(
+		token: string,
+		instance: TelegramBase
+	): Promise<void> {
+		this.clients.set(instance.client, instance);
+
+		if (this.inited) return;
+		this.url = "https://api.telegram.org/bot" + token + "/";
+		this.globalAbort = new AbortController();
+		this.inited = true;
+		this.subscribe();
+
+		const { username } = await this.call("getMe");
+		this.username = username;
+	}
+
+	protected static close(): void {
+		this.inited = false;
+		this.globalAbort.abort();
 	}
 
 	protected static async call(
 		method: string,
 		params: Record<string, any> = {},
 		abortController?: AbortController
-	): Promise<Response> {
+	): Promise<Record<string, any>> {
+		if (!this.inited) return {};
+
 		const mixedAbort = new AbortController();
-		this.abortController.signal.addEventListener("abort", (): void => {
+		this.globalAbort.signal.addEventListener("abort", () => {
 			mixedAbort.abort();
 		});
-		abortController?.signal.addEventListener("abort", (): void => {
+		abortController?.signal.addEventListener("abort", () => {
 			mixedAbort.abort();
 		});
 
-		return fetch(this.url + method, {
+		const response = await fetch(this.url + method, {
 			method: "POST",
 			headers: Form.headers,
 			body: new Form(params),
 			signal: mixedAbort.signal
-		}).catch(e => {
-			return {
-				status: 503,
-				text: (): string => e.toString(),
-				json: () => null,
-				ok: false
-			} as any;
+		}).catch(error => {
+			const text = JSON.stringify(error);
+			throw new Error(`Failed to execute "${method}"!\n${text}`);
 		});
-	}
 
-	protected static initialize(token: string, instance: TelegramBase): void {
-		this.clients.set(instance.client, instance);
-		if (this.inited) return;
-		this.url = "https://api.telegram.org/bot" + token + "/";
-		this.abortController = new AbortController();
-		this.inited = true;
-		this.subscribe();
-	}
+		const data = await response.json().catch(async error => {
+			throw new Error(
+				`JSON parse failed upon "${method}" execution!\n${await response.text()}`
+			);
+		});
 
-	protected static close(): void {
-		this.inited = false;
-		this.abortController.abort();
+		if (!("result" in data)) {
+			const text = JSON.stringify(data);
+			throw new Error(`Invalid data recieved!\n${text}`);
+		}
+
+		return data.result;
 	}
 
 	private static async subscribe(offset = 0): Promise<void> {
 		if (!this.inited) return;
-		const response = await this.call("getUpdates", {
-			offset,
-			timeout: 30
-		});
+		const { data, error, status } = await gretch(this.url + "getUpdates", {
+			signal: this.globalAbort.signal,
+			timeout: 120 * 1000,
+			method: "POST",
+			json: {
+				offset,
+				timeout: 60
+			}
+		}).json();
 		if (!this.inited) return;
 
 		//Timeout
-		if (response.status == 502) {
+		if (status === 502) {
 			this.subscribe(offset);
 			return;
 		}
 		//Error
-		if (response.status != 200) {
-			log(
-				`Error code ${
-					response.status
-				} recieved on polling\n${await response.text()}`,
-				LogType.ERROR
-			);
+		if (error) {
+			const text = JSON.stringify(error);
+			err(`Error code ${status} recieved on polling\n${text}`);
 
 			sleep(1000);
 			this.subscribe(offset);
 			return;
 		}
-
-		//Update
-		const updates = (await response.json())["result"];
-		if (!Array.isArray(updates)) {
-			log(`Unknown update data ${updates} recieved!`, LogType.ERROR);
+		//Validate
+		if (!data.result || !Array.isArray(data.result)) {
+			const text = JSON.stringify(data);
+			err(`Invalid update data recieved!\n${text}`);
 
 			await sleep(1000);
 			this.subscribe(offset);
 			return;
 		}
 
-		for (const update of updates) {
-			this.update(update);
+		for (const update of data.result) {
+			if (!update["update_id"]) continue;
+			this.update(update).catch(e =>
+				err(`Failed to process update!\n${e}`)
+			);
 			offset = Math.max(offset, update["update_id"] + 1);
 		}
 		this.subscribe(offset);
 	}
 
 	private static async check(channel: number): Promise<TelegramBase | null> {
-		const adminsInfo = await this.call("getChatAdministrators", {
+		const admins = await this.call("getChatAdministrators", {
 			chat_id: channel
 		});
 
-		const admins = (await adminsInfo.json())["result"];
 		if (Array.isArray(admins)) {
 			for (const admin of admins) {
 				const client = this.clients.get(admin.user.id);
@@ -130,73 +161,138 @@ export default abstract class TelegramBase extends Endpoint {
 	private static async update(data: Record<string, any>): Promise<void> {
 		if (data["message"]) {
 			const message = data["message"];
-			const sender = message["from"]?.["id"];
-			const name = message["from"]?.["username"];
-			if (!+sender) return;
-			const client = this.clients.get(+sender);
+			if (!is<IMessage>(message)) {
+				wrn(`Unkown message recieved!\n${JSON.stringify(message)}`);
+				return;
+			}
+
+			const { id, username } = message.from;
+			const client = this.clients.get(id);
 			if (!client) {
-				log(
-					`Unauthorized access attempt from @${name} (${sender})!`,
-					LogType.WARNING
-				);
+				wrn(`Unauthorized access attempt from @${username} (${id})!`);
 				return;
 			}
 
 			const text =
-				message["text"] ||
-				[message.audio.performer, message.audio.title]
+				message.text ||
+				[message.audio?.performer, message.audio?.title]
 					.filter(x => x)
 					.join(" - ");
 
 			if (!text) return;
 
-			if (text[0] == "/") client.onCommand(text.slice(1));
-			else client.onMessage(text);
-
 			this.call("deleteMessage", {
-				chat_id: sender,
-				message_id: message["message_id"]
+				chat_id: id,
+				message_id: message.message_id
 			});
+
+			if (text[0] == "/") await client.onCommand(text.slice(1));
+			else await client.onMessage(text);
 		} else if (data["my_chat_member"]) {
-			const update = data["my_chat_member"];
-			const member = update["new_chat_member"];
-			const chat = update.chat.id;
-			const title = update.chat.title;
+			const member = data["my_chat_member"];
+			if (!is<IChatMember>(member)) {
+				wrn(`Unkown member recieved!\n${JSON.stringify(member)}`);
+				return;
+			}
+			if (member.new_chat_member.status !== "administrator") return;
 
-			if (!member) return;
-			if (member.status === "left") return;
-
-			const chatInfo = await this.call("getChat", { chat_id: chat });
-
-			const client = await this.check(chat);
-			const info = (await chatInfo.json())["result"];
-			if (client) {
-				client.onChat(chat, title, info.description);
+			const { id, title } = member.chat;
+			const client = await this.check(id);
+			if (!client) {
+				wrn(`Unauthorized "${title}" (${id}) playlist access!`);
+				await this.call("leaveChat", { chat_id: id });
 				return;
 			}
 
-			await this.call("leaveChat", { chat_id: chat });
-			log(
-				`Unauthorized "${title}" (${chat}) playlist access!`,
-				LogType.WARNING
-			);
+			const { description } = await this.call("getChat", { chat_id: id });
+			await client.onChat(id, title, description);
 		} else if (data["channel_post"]) {
 			const post = data["channel_post"];
-			const chat = post.chat?.id;
+			if (!is<IPost>(post)) {
+				wrn(`Unkown post recieved!\n${JSON.stringify(post)}`);
+				return;
+			}
+
+			const { id, title } = post.chat;
 			const text = post.text;
+			const audio = [post.audio?.performer, post.audio?.title]
+				.filter(x => x)
+				.join(" - ");
 
-			if (!chat) return;
-			if (!text || text[0] !== "/") return;
-
-			const client = await this.check(chat);
+			const client = await this.check(id);
 			if (!client) return;
 
-			client.onCommand(text.slice(1));
-
 			this.call("deleteMessage", {
-				chat_id: chat,
-				message_id: post["message_id"]
+				chat_id: id,
+				message_id: post.message_id
 			});
+
+			if (text?.includes(`@${this.username}`)) {
+				await client.onTagged(title);
+			} else if (audio) {
+				await client.onPost(audio, title);
+			}
+		} else if (data["callback_query"]) {
+			const query = data["callback_query"];
+			if (!is<IQuery>(query)) {
+				wrn(`Unkown post recieved!\n${JSON.stringify(query)}`);
+				return;
+			}
+
+			const { id, username } = query.from;
+			const client = this.clients.get(id);
+			if (!client) {
+				wrn(`Unauthorized access attempt from @${username} (${id})!`);
+				return;
+			}
+
+			this.call("answerCallbackQuery", {
+				callback_query_id: query.id
+			}).catch(() => {});
+			try {
+				const parsed = JSON.parse(query.data);
+				assertType<ICallbackData>(parsed);
+				await client.onCallback(
+					parsed,
+					query.id,
+					query.message.message_id,
+					id
+				);
+			} catch {
+				//Ignore errors here
+			}
 		}
 	}
+}
+
+export interface ICallbackData {
+	type: string;
+	query?: string;
+	source?: string;
+}
+
+interface IMessage {
+	from: { id: number; username: string };
+	message_id: number;
+	text?: string;
+	audio?: { performer?: string; title: string };
+}
+
+interface IChatMember {
+	new_chat_member: { status: string };
+	chat: { id: number; title: string };
+}
+
+interface IPost {
+	chat: { id: number; title: string };
+	message_id: number;
+	text?: string;
+	audio?: { performer?: string; title: string };
+}
+
+interface IQuery {
+	id: string;
+	from: { id: number; username: string };
+	message: { message_id: number };
+	data: string;
 }
