bfe00d8a91c47bfe08aa95a9893a515c19fa2694
Azarattum <azarattum@pm.me>
2021-04-29 22:52:52 +0700

    Updated dependencies. Adjusted configs

--- typing.d.ts
+++ typing.d.ts
@@ -1,9 +1,21 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { RequestInfo, RequestInit, Response } from "node-fetch";
+import * as nf from "node-fetch";
+import * as ac from "abort-controller";
 /** Custom typings */
 
 declare global {
-	function fetch(url: RequestInfo, init?: RequestInit): Promise<Response>;
+	function fetch(
+		url: nf.RequestInfo,
+		init?: nf.RequestInit
+	): Promise<nf.Response>;
+
+	type RequestInfo = nf.RequestInfo;
+	type RequestInit = nf.RequestInit;
+	type Response = nf.Response;
+	type Request = nf.Request;
+	type BodyInit = nf.BodyInit;
+	type AbortController = ac.AbortController;
+
 	type func = (...args: any[]) => any;
 	type obj = Record<any, any>;
 }
