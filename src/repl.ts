b1455f1aebef9ab5e6dbd54a7179ebceb5be714a
Azarattum <azarattum@pm.me>
2021-05-30 13:05:36 +0700

    Updated repl setup

--- src/repl.ts
+++ src/repl.ts
@@ -1,9 +1,22 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import * as _ from "../typing";
 import DotEnv from "dotenv";
 import * as fetch from "node-fetch";
+import { URL } from "url";
 
 //Setup env
 DotEnv.config();
-Object.assign(globalThis, { ...fetch, fetch });
+Object.assign(globalThis, {
+	...fetch,
+	fetch: (...args: any[]) => {
+		console.log("Requesting " + new URL(args[0].url).toString());
+		const r = fetch.default(args[0], args[1]);
+		r.then(x => {
+			console.log("Done " + new URL(args[0].url).host, x.status);
+		});
+		return r;
+	}
+});
 
 //Async closure
 (async () => {
