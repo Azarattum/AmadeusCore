0d49ba11c9d9c6e6bdcb5d777dfc2dc2cfa1a649
Azarattum <azarattum@pm.me>
2021-11-14 23:05:56 +0700

    Updated dependencies

--- src/repl.ts
+++ src/repl.ts
@@ -9,10 +9,15 @@ DotEnv.config();
 Object.assign(globalThis, {
 	...fetch,
 	fetch: (...args: any[]) => {
-		console.log("Requesting " + new URL(args[0].url).toString());
+		const url =
+			typeof args[0] === "string"
+				? args[0]
+				: new URL(args[0].url).toString();
+
+		console.log("Requesting " + url);
 		const r = fetch.default(args[0], args[1]);
 		r.then(x => {
-			console.log("Done " + new URL(args[0].url).host, x.status);
+			console.log("Done " + new URL(url).host, x.status);
 		});
 		return r;
 	}
